3a9d921b6dc1eae078115449a3790b2ed56caf46
曽 佳宸 <jiachen@akabeesoft2.com>
2024-09-24 17:03:00 +0900

    FEAT: Test ReaPack stuff

---
+++ Playground/jiachen_test reapack.lua
@@ -0,0 +1,9 @@
+-- @description Test ReaPack
+-- @author Jiachen
+-- @version 1.0.0
+-- @about
+-- Just to ...
+-- test if I can use ReaPack.
+-- [Test the link to my repo](https://github.com/JiaChen-Zeng/ReaScripts)
+
+local a = 1
